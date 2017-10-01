9afc89f8b35b5c3c0e2c9071dc96898c493fee05
Ilia Aderiha <aderihojustdev@gmail.com>
2017-10-01 22:05:25 +0300

    fix

--- packages/server/src/service/PictureStorageService.js
+++ packages/server/src/service/PictureStorageService.js
@@ -1,14 +1,23 @@
 import Promise from 'bluebird';
+import config from 'config';
+import deasync from 'deasync';
+import Backblaze from 'backblaze-b2';
 import shortid from 'shortid';
-
 export default class PictureStorageService{
 
     constructor(){
-
+        this.accountId = config.get('backblaze.account_id');
+        this.applicationKey = config.get('backblaze.application_key');
+        //this.b2 = Promise.promisifyAll(new Backblaze({ accountId: this.accountId, applicationKey: this.applicationKey }));
+        //deasync(this.b2.authorize)();
     }
 
     save = () => {
         return Promise.resolve(shortid.generate());
+    };
+
+    remove = () => {
+
     };
 
 }
